7db8dbd08e0983ccb52d34deb3f16f96e0defde4
ngochan1301 <hanxinhh1301@gmail.com>
2025-02-15 12:02:54 +0700

    Initial commit

---
+++ src/main/java/com/group3/skincare_service_booking/SkincareServiceBookingApplication.java
@@ -0,0 +1,13 @@
+package com.group3.skincare_service_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkincareServiceBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkincareServiceBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group3/skincare_service_booking/SkincareServiceBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.skincare_service_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkincareServiceBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
